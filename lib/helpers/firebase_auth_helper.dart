8ddeb489d488dd620dd1595f9dff9ca164e54934
Abenezer_Tadesse <abenidev@gmail.com>
2024-12-13 15:55:20 +0300

    add login demo ui

--- lib/helpers/firebase_auth_helper.dart
+++ lib/helpers/firebase_auth_helper.dart
@@ -20,21 +20,19 @@ class FirebaseAuthHelper {
     });
   }
 
-  static Future<UserCredential> signInWithGoogle() async {
-    // Trigger the authentication flow
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-
-    // Obtain the auth details from the request
-    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
-
-    // Create a new credential
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth?.accessToken,
-      idToken: googleAuth?.idToken,
-    );
-
-    // Once signed in, return the UserCredential
-    return await FirebaseAuth.instance.signInWithCredential(credential);
+  static Future<bool> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+      await FirebaseAuth.instance.signInWithCredential(credential);
+      return true;
+    } catch (e) {
+      return false;
+    }
   }
 
   static Future<void> signOut() async {
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,15 @@
 import 'package:budgetor/helpers/firebase_auth_helper.dart';
-import 'package:budgetor/main.dart';
 import 'package:budgetor/screens/home_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lottie/lottie.dart';
+import 'package:sign_in_button/sign_in_button.dart';
+
+final isSignInBtnLoadingProvider = StateProvider<bool>((ref) {
+  return false;
+});
 
 class LoginScreen extends ConsumerStatefulWidget {
   const LoginScreen({super.key});
@@ -16,20 +21,74 @@ class LoginScreen extends ConsumerStatefulWidget {
 class _LoginScreenState extends ConsumerState<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            UserCredential userCredential = await FirebaseAuthHelper.signInWithGoogle();
-            logger.i(userCredential);
+    bool isSignInBtnLoading = ref.watch(isSignInBtnLoadingProvider);
 
-            if (FirebaseAuth.instance.currentUser != null && context.mounted) {
-              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomeScreen()));
-            }
-          },
-          child: Text(
-            'Signin with Google',
-            style: TextStyle(fontSize: 12.sp),
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 25.w),
+          child: Column(
+            children: [
+              SizedBox(height: 20.h),
+              RichText(
+                text: TextSpan(
+                  text: '',
+                  style: Theme.of(context).textTheme.headlineLarge?.copyWith(fontSize: 26.sp),
+                  children: const <TextSpan>[
+                    TextSpan(text: 'Take Control ', style: TextStyle(fontWeight: FontWeight.bold)),
+                    TextSpan(text: 'of your finance Today!'),
+                  ],
+                ),
+              ),
+              SizedBox(height: 40.h),
+              SizedBox(
+                width: 310.w,
+                height: 300.h,
+                child: Lottie.asset('assets/anims/bar_chart_1.json'),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Get a clear picture of where your money is going to make it easier to save for what truly matters. No need to be a finance expert. The app does the math for you!',
+                  style: TextStyle(fontSize: 12.sp),
+                ),
+              ),
+              SizedBox(height: 10.h),
+              const Spacer(),
+              Row(
+                children: [
+                  Expanded(
+                    child: SignInButton(
+                      Buttons.google,
+                      mini: false,
+                      elevation: isSignInBtnLoading ? 0.0 : 2.0,
+                      padding: EdgeInsets.symmetric(vertical: 3.h),
+                      onPressed: () async {
+                        if (isSignInBtnLoading) return;
+                        ref.read(isSignInBtnLoadingProvider.notifier).state = true;
+                        bool isSignedIn = await FirebaseAuthHelper.signInWithGoogle();
+                        ref.read(isSignInBtnLoadingProvider.notifier).state = false;
+                        if (FirebaseAuth.instance.currentUser != null && isSignedIn && context.mounted) {
+                          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+                        }
+                      },
+                    ),
+                  ),
+                  if (isSignInBtnLoading) ...[
+                    SizedBox(width: 20.w),
+                    SizedBox(
+                      width: 20.w,
+                      height: 20.h,
+                      child: CircularProgressIndicator(
+                        strokeWidth: 3.w,
+                        strokeCap: StrokeCap.round,
+                      ),
+                    ),
+                  ]
+                ],
+              ),
+              SizedBox(height: 30.h),
+            ],
           ),
         ),
       ),
